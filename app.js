d6aa33bcf7db3ff92ec23793a07def2da7e49b75
shouvik mandal <shouvik0013@gmail.com>
2021-09-22 18:36:53 +0530

    some changes in app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const http = require('http');
-const routes = require('./routes');
 
 
-console.log(routes.routeText);
-const server = http.createServer(routes.reqHandler);
+const express = require('express');
+
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the first middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In the 2nd middleware')
+})
+
+
+// here app is also a valid request handler
+const server = http.createServer(app);
 
 server.listen(3000);
