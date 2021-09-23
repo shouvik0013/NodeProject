4b0e57747bbd45d3dd8a58543fb297a7b2f5ae7d
shouvik mandal <shouvik0013@gmail.com>
2021-09-23 16:24:25 +0530

    app.listen() is used now

--- app.js
+++ app.js
@@ -12,11 +12,14 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    console.log('In the 2nd middleware')
+    console.log('In the 2nd middleware');
+    res.send('<h1>Hello from Express</h1>');
 })
 
 
 // here app is also a valid request handler
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
+
+app.listen(3000);
