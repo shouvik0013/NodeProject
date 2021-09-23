0c8d244c9b4b7be7dc59403d6daba57cf0a5b10f
shouvik0013 <shouvik0013@gmail.com>
2021-09-23 19:15:06 +0530

    bodyparser added, new middleware added

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const http = require('http');
-
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the first middleware');
-    next();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In the 2nd middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
     console.log('In the 2nd middleware');
     res.send('<h1>Hello from Express</h1>');
-})
+});
 
 
 // here app is also a valid request handler
