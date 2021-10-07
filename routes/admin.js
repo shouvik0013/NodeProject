acfc7e0ea18893469dee43100399511783e94e25
shouvik0013 <shouvik0013@gmail.com>
2021-10-07 11:10:52 +0530

    white spaces added

--- routes/admin.js
+++ routes/admin.js
@@ -3,19 +3,19 @@ const path = require('path');
 
 
 // local modules
-const rootDir = require('../utils/path');
+const rootDir = require('../utils/path');   // supplies the dirname of app.js
 
 // third party modules
-const app = require('express');
+const express = require('express');
 
 
-const router = app.Router();
+const router = express.Router();
 
 // to store products data
 const products = [];
 
 
-// path: /admin/add-product => 
+// path: /admin/add-product 
 router.get('/add-product', (req, res, next) => {
     // console.log('In the 2nd middleware');
     // res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
