49d0c8d77bf62c4f1acfc1a0ae816a49e1b9f70d
shouvik0013 <shouvik0013@gmail.com>
2021-10-22 18:51:20 +0530

    working 404 page

--- controllers/404_Controller.js
+++ controllers/404_Controller.js
@@ -1,4 +1,4 @@
 module.exports = (req, res, next) => {
   // res.status(404).send('<h1>Requested page not found</h1>');
-  res.status(404).render("404", { pageTitle: "Page not found" });
+  res.status(404).render("404", { pageTitle: "Page not found", path: "/" });
 };
