19c9e53f537be24b849d59bb8ef9270acf4e1cb9
shouvik0013 <shouvik0013@gmail.com>
2021-10-31 12:35:48 +0530

    fetchAll method of product.js is fixed

--- controllers/products.js
+++ controllers/products.js
@@ -1,29 +1,30 @@
-const Product = require("../models/product");
+const Product = require("../models/product"); // Product holds a class
 
 module.exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    activeAddProduct: true,
-    formsCSS: true,
-    productCSS: true,
-  });
+    res.render("add-product", {
+        pageTitle: "Add Product",
+        path: "/admin/add-product",
+        activeAddProduct: true,
+        formsCSS: true,
+        productCSS: true,
+    });
 };
 
 module.exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect("/");
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect("/");
 };
 
 module.exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0 ? true : false,
-    activeShop: true,
-    productCSS: true,
-  });
+    const products = Product.fetchAll((products) => {
+        res.render("shop", {
+            prods: products,
+            pageTitle: "Shop",
+            path: "/",
+            hasProducts: products.length > 0 ? true : false,
+            activeShop: true,
+            productCSS: true,
+        });
+    });
 };
--- models/product.js
+++ models/product.js
@@ -1,37 +1,40 @@
 // core modules
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+const path = require("path");
 
 // third-party modules
 
 // local modules
-const rootDir = require('../utils/path');
+const rootDir = require("../utils/path");
 
 module.exports = class Product {
-  constructor(title) {
-    this.title = title;
-  }
+    constructor(title) {
+        this.title = title;
+    }
 
-  save() {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      let products = new Array();
-      if(!err) {
-        products = JSON.parse(fileContent);
-      }
-    
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
-    });
-  }
+    save() {
+        const p = path.join(rootDir, "data", "products.json");
+        fs.readFile(p, (err, fileContent) => {
+            let products = new Array();
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
 
-  static fetchAll() {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
 
-    });
-  }
+    static fetchAll(cb) {
+        // here cb is a callback function
+        const p = path.join(rootDir, "data", "products.json");
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
+    }
 };
